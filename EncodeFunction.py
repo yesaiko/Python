f60d7688073d8b360f9c526e5fbe05f3d56809e8
yesaiko <xxxyesaiko@gmail.com>
2020-10-21 22:12:19 +0200

    Replace multiple characters by another multiple characters in a string and changing all toUpper.

--- EncodeFunction.py
+++ EncodeFunction.py
@@ -16,10 +16,10 @@ def encode (text_before, to_replace, replaced):
             index_number_list = to_replace.index(every_char)
             text_before = text_before.replace(every_char, replaced[index_number_list])
 
-    return text_before
+    return text_before.upper()
 
 text = 'Is Zed Eating All Strings Texts Before Oatmeal'
 text_after = encode(text, ['I','Z','E','A','S','T','B','O'], ['1','2','3','4','5','7','8','0'])
 
 print('String before replace: ', text)
-print('String after replace: ',text_after)
+print('String after replace: ', text_after)
